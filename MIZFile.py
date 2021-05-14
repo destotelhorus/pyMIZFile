4cd664b94368b699e9994c7351e3cae7d92a6bfc
dth@dthweb.net <dth@dthweb.net>
2021-05-14 08:30:34 +0200

    Added more theatre support. Added coordinate transformation functions.

--- MIZFile.py
+++ MIZFile.py
@@ -4,6 +4,7 @@ from ruamel.std.zipfile import InMemoryZipFile
 from zipfile import ZipFile
 from .libraries.slpp import dcsslpp as lua
 from datetime import datetime
+from pyproj import Proj
 
 WRITEACCESS_ERROR = 'This file is write-protected!'
 class WriteProtectionError(Exception):
@@ -15,6 +16,38 @@ class MIZFile(object):
     missionData = None
     missionLua = None
     theatre = None
+    projection = None
+    projectiondata = {}
+    projectiondata['Caucasus'] = {}
+    projectiondata['Caucasus']['utmzone'] = "36"
+    projectiondata['Caucasus']['northing'] = 4998115
+    projectiondata['Caucasus']['easting'] = 599517
+    projectiondata['Caucasus']['hemi'] = 'north'
+    projectiondata['Nevada'] = {}
+    projectiondata['Nevada']['utmzone'] = "11"
+    projectiondata['Nevada']['northing'] = 4410028.064
+    projectiondata['Nevada']['easting'] = 693996.81
+    projectiondata['Nevada']['hemi'] = 'north'
+    projectiondata['Normandy'] = {}
+    projectiondata['Normandy']['utmzone'] = "30"
+    projectiondata['Normandy']['northing'] = 5484813
+    projectiondata['Normandy']['easting'] = 695526
+    projectiondata['Normandy']['hemi'] = 'north'
+    projectiondata['PersianGulf'] = {}
+    projectiondata['PersianGulf']['utmzone'] = "40"
+    projectiondata['PersianGulf']['northing'] = 2894932.9363
+    projectiondata['PersianGulf']['easting'] = 424243.9786
+    projectiondata['PersianGulf']['hemi'] = 'north'
+    projectiondata['Syria'] = {}
+    projectiondata['Syria']['utmzone'] = "37"
+    projectiondata['Syria']['northing'] = 3879865
+    projectiondata['Syria']['easting'] = 217198
+    projectiondata['Syria']['hemi'] = 'north'
+    projectiondata['TheChannel'] = {}
+    projectiondata['TheChannel']['utmzone'] = "31"
+    projectiondata['TheChannel']['northing'] = 5636888
+    projectiondata['TheChannel']['easting'] = 400623
+    projectiondata['TheChannel']['hemi'] = 'north'
 
     def __init__(self, filename, readonly=True):
         self.MIZfilename = filename
@@ -50,10 +83,32 @@ class MIZFile(object):
         if self.theatre:
             return self.theatre
         mizfilehandle = ZipFile(self.MIZfilename, mode='r')
-        theatrefilehandle = mizfilehandle.open('theatre', 'r')
-        self.theatre = theatrefilehandle.read().decode('UTF-8')
+        try:
+            theatrefilehandle = mizfilehandle.open('theatre', 'r')
+            self.theatre = theatrefilehandle.read().decode('UTF-8')
+        except KeyError:
+            #Could be the newer MIZ format where the theatre file is no longer extra
+            self.theatre = self.getMission()["theatre"]
         return self.theatre
+    
+    def getProjectionData(self):
+        return self.projectiondata[self.getTheatre()]
+        
+    def getProjection(self):
+        if self.projection:
+            pass
+        else:
+            self.projection = Proj("+proj=utm +zone="+self.getProjectionData()['utmzone']+",+"+self.getProjectionData()['hemi']+" +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+        return self.projection
 
+    def getProjectedXY(self, lat, lon):
+        y, x = self.getProjection()(lon, lat)
+        return (x-self.getProjectionData()['northing'],y-self.getProjectionData()['easting'])
+        
+    def getProjectedLatLon(self, x, y):
+        lon, lat = self.getProjection()(y+self.getProjectionData()['easting'], x+self.getProjectionData()['northing'],inverse=True)
+        return (lat, lon)
+        
     def getTheatreLatLon(self):
         if self.getTheatre() == 'Caucasus':
             return {"lat": 42.355691, "lon": 43.323853}
@@ -63,6 +118,10 @@ class MIZFile(object):
             return {"lat": 36.145615, "lon": -115.187618}
         elif self.getTheatre() == 'Normandy':
             return {"lat": 49.183336, "lon": -0.365908}
+        elif self.getTheatre() == 'Syria':
+            return {"lat": 35.140901, "lon": 36.051701}
+        elif self.getTheatre() == 'TheChannel':
+            return {"lat": 50.945076, "lon": -0.365908}
         else:
             return None
 
